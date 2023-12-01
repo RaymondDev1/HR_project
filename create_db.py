953b953210bcb2a2964c66b2632f21ee96792468
RaymondDev1 <150017963+RaymondDev1@users.noreply.github.com>
2023-12-01 20:59:08 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -28,6 +28,6 @@ def postgre_query(connection, query):
 connect_postgre = create_connection("postgres", "postgres", "jerry1", "127.0.0.1", "5432")
 postgre_query(connect_postgre, "create database hr")
 connect_postgre = create_connection("hr", "postgres", "jerry1", "127.0.0.1", "5432")
-pos*tgre_query(connect_postgre,"create table positions (position_code varchar(3) primary key, position_name varchar(30))")
+postgre_query(connect_postgre,"create table positions (position_code varchar(3) primary key, position_name varchar(30))")
 postgre_query(connect_postgre, "create table employer (emp_id serial primary key, fname varchar(15), lname varchar(25), mname varchar(20), position_code varchar(3), start_date varchar(10))")
 postgre_query(connect_postgre, "alter table employer add constraint emp_pos foreign key (position_code) references positions(position_code)")
